396e2bc3cb8f56ba1c8973224f59e9f232814a2a
Jas Singh <jaskiratpal.singh@outlook.com>
2025-01-07 01:42:19 -0800

    Remove copilot integration as that can be installed if needed.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,8 +24,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.util.dot" },
-    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
-    { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.json" },
